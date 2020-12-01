da72acf03ade8dc6398cbc589e0aea15d300e38e
fabiomarianidev <70094393+fabiomarianidev@users.noreply.github.com>
2020-12-01 22:15:40 +0100

    Create unified Input component Component is used both to create and edit notes

---
+++ src/components/InputModal.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+
+// Modal used for creating a new note and editing an existing one
+// props are:
+// active: tells if the modal is visible
+// content: if it exists (component is called to edit), it contains the current note text
+// handleConfirmButton: function that receives the new or updated string when the confirmation button is pressed. It must set "active" to false.
+// handleCloseButton: function that handles the pressing of the close button. It must set "active" to false
+// if props.content exists, the component is called to edit a note:
+// 
+
+export default function InputModal (props) {
+    const [content, setContent] = useState();
+
+    useEffect( () => {
+        if (props.content) {
+            setContent(props.content);
+        } else {
+            setContent("");
+        }
+    }, [props.content] )
+
+    const handleTextareaChange = (event) => {
+        setContent(event.target.value);
+    }
+
+    const handleConfirmButton = () => {
+        if (content !== "") {
+            props.handleConfirmButton(content);
+        }
+        setContent("");
+    }
+
+    const handleClearButton = () => {
+        setContent("");
+    }
+
+    const handleCloseButton = () => {
+        props.handleCloseButton();
+    }
+
+    return(
+        <div className={props.active ? "modal is-active" : "modal"}>
+        <div className="modal-background"></div>
+        <div className="modal-content">
+            <div className="card p-5">
+                    <textarea className="textarea" rows="5" value={content} onChange={handleTextareaChange} />
+                    <button className="button is-primary mt-3 mr-2" onClick={handleConfirmButton} >
+                        <i className="fas fa-check" />
+                    </button>
+                    <button className="button is-info mx-2 mt-3" onClick={handleClearButton} >
+                        <i className="fas fa-eraser" />
+                    </button>
+                    <button className="button is-danger mx-2 mt-3" onClick={handleCloseButton}>
+                        <i className="fas fa-times" />
+                    </button>
+            </div>
+        </div>
+    </div>
+    )
+} 
--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,67 +1,45 @@
 import { useState } from "react";
 
+import InputModal from "./InputModal";
+
 export default function NoteCard (props) {
 
     const [ editMode, setEditMode ] = useState(false);
-    const [ newNoteContent, setNewNoteContent ] = useState(props.content);
-    
-    const handleDelete = () => {
-        props.deleteNote(props.id);
-        console.log(props.id)
-    }
-
+ 
     const handleEditButton = () => {
-        setNewNoteContent(props.content);
         setEditMode(true);
     }
 
-    const handleTextareaChange = (event) => {
-        setNewNoteContent(event.target.value);
+    const handleDelete = () => {
+        props.deleteNote(props.id);
     }
 
-    const handleAddButton = () => {
-        if (newNoteContent !== "") {
-            props.editNote(props.id, newNoteContent);
-            setEditMode(false);
-        }
+    const handleConfirm = (newContent) => {
+        props.editNote(props.id, newContent);
+        setEditMode(false);
     }
 
-    const handleClearButton = () => {
-        setNewNoteContent("");
-    }
-
-    const handleCloseButton = () => {
+    const handleClose = () => {
         setEditMode(false);
     }
 
     return(
         <>
-        <div className="card notecard__container">
-            <div className="card-content">
-                <div className="content notecard__content">
-                    {props.content}
-                </div>
-                <button className="delete is-medium has-background-danger notecard__button" onClick={handleDelete}/>
-                <button className="button is-info is-small" onClick={handleEditButton}><i className="fas fa-edit"/></button>
-            </div>
-        </div>
-        <div className={editMode ? "modal is-active" : "modal"}>
-            <div className="modal-background"></div>
-            <div className="modal-content">
-                <div className="card p-5">
-                        <textarea className="textarea" rows="5" value={newNoteContent} onChange={handleTextareaChange} />
-                        <button className="button is-primary mt-3 mr-2" onClick={handleAddButton} >
-                            <i className="fas fa-check" />
-                        </button>
-                        <button className="button is-info mx-2 mt-3" onClick={handleClearButton} >
-                            <i className="fas fa-eraser" />
-                        </button>
-                        <button className="button is-danger mx-2 mt-3" onClick={handleCloseButton}>
-                            <i className="fas fa-times" />
-                        </button>
+            <div className="card notecard__container">
+                <div className="card-content">
+                    <div className="content notecard__content">
+                        {props.content}
+                    </div>
+                    <button className="delete is-medium has-background-danger notecard__button" onClick={handleDelete}/>
+                    <button className="button is-info is-small" onClick={handleEditButton}><i className="fas fa-edit"/></button>
                 </div>
             </div>
-        </div>
+            <InputModal 
+                active={editMode}
+                content={props.content}
+                handleConfirmButton={handleConfirm}
+                handleCloseButton={handleClose}
+            />
         </>
     )
 } 
